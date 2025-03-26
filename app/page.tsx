966be55b3c0fd15983aac43c6c501e89cd93972f
Mubashar217 <muhammadmubasharoffical@gmail.com>
2025-03-26 13:53:52 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './components/sidebar/Sidebar'
+
+
+export default function page() {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+    
+  )
+}
