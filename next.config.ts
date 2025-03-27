032f7936b6381dc407db9a249f8bc298a3794511
Mubashar217 <muhammadmubasharoffical@gmail.com>
2025-03-27 13:55:18 +0500

    config change

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+      ignoreDuringBuilds: true,
   },
-  plugins: [],
+
 }
+
+module.exports = nextConfig
